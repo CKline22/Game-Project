d1a6ef770e45dbad362ceca059baccc10d3d06cb
CKline22 <CKline.1022@gmail.com>
2022-07-30 16:12:57 -0400

    javascrit mock code

---
+++ src/app.js
@@ -0,0 +1,37 @@
+
+//let jumping = false
+
+// function action(x) {
+//     if(x.keycode === keycode#){
+    //if(!jumping) {
+        //jumping = true
+        //jump()
+    //}
+//     }
+// }
+
+// function jump()
+//  let position = 0
+//  let timer = setInterval(function (){
+
+//move down
+// if (position === 120){
+//  clearInterval(timer)
+//  console.log('down')
+//  let timerdown = setInterval(function (){
+    //if (position === 0) {
+        //clearInterval(timerdown)
+        //jumping = false
+    //}
+    //position -= 30
+    //dino.style.bottom = position + 'px'
+//},20)
+
+//move up
+//console.log('up)
+//position += 30
+//dino.style.bottom = position + 'px'
+//},20)
+//})
+
+
