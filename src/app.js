b54c230da2df24e91c5ad4fc5cc660a82d27782b
CKline22 <CKline.1022@gmail.com>
2022-08-01 20:05:39 -0400

    click jump function

--- src/app.js
+++ src/app.js
@@ -1,37 +1,25 @@
+const ball = document.getElementsByClassName("ball")
 
-//let jumping = false
 
-// function action(x) {
-//     if(x.keycode === keycode#){
-    //if(!jumping) {
-        //jumping = true
-        //jump()
-    //}
-//     }
-// }
+document.addEventListener("click", e => {
+    console.log("clicked")
+    jump()
+})
 
-// function jump()
-//  let position = 0
-//  let timer = setInterval(function (){
 
-//move down
-// if (position === 120){
-//  clearInterval(timer)
-//  console.log('down')
-//  let timerdown = setInterval(function (){
-    //if (position === 0) {
-        //clearInterval(timerdown)
-        //jumping = false
-    //}
-    //position -= 30
-    //ball.style.bottom = position + 'px'
-//},20)
+function jump () {
+    let position = 0
+    let timer = setInterval(function () {
+        
+        
+        
+        //jump up
+        console.log("jump up")
+        position += 30
+        ball.style.bottom = position + 'px'
+    },20)
+}
 
-//move up
-//console.log('up)
-//position += 30
-//ball.style.bottom = position + 'px'
-//},20)
-//})
+
 
 
